3130411b5c682b2b1091293f5765bdea2e82cbc6
decanus <dean.eigenmann@icloud.com>
2019-03-02 19:38:23 +0100

    updated

---
+++ contracts/Registrar.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+import "./RegistrarInterface.sol";
+import "@ensdomains/ens/contracts/ENS.sol";
+
+contract Registrar is RegistrarInterface {
+
+    ENS public ens;
+    bytes32 public node;
+
+    constructor(ENS _ens, bytes32 _node) public {
+        ens = _ens;
+        node = _node;
+    }
+
+    function register(bytes32 name, address owner, bytes signature) external {
+
+    }
+}
--- contracts/RegistrarInterface.sol
+++ contracts/RegistrarInterface.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.0;
 
 interface RegistrarInterface {
 
     event Registration(bytes32 indexed name, address owner, address addr);
 
-    function register(bytes32 name, address owner, bytes signature);
+    function register(bytes32 name, address owner, bytes signature) external;
 }
