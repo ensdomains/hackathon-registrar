2e475b9e6a1068d9872e17ffb98d1bf618e8abe3
decanus <dean.eigenmann@icloud.com>
2019-03-02 20:18:10 +0100

    fix

--- contracts/RegistrarInterface.sol
+++ contracts/RegistrarInterface.sol
@@ -4,5 +4,5 @@ interface RegistrarInterface {
 
     event Registration(bytes32 indexed name, address owner, address addr);
 
-    function register(bytes32 name, address owner, bytes signature) external;
+    function register(string name, address owner, bytes signature) external;
 }
