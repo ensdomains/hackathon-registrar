6a568a83db0c82947d77c155edb0280efcb7e0bb
decanus <dean.eigenmann@icloud.com>
2019-03-03 08:41:13 +0100

    hash fuunc

--- contracts/RegistrarInterface.sol
+++ contracts/RegistrarInterface.sol
@@ -5,4 +5,5 @@ interface RegistrarInterface {
     event Registration(bytes32 indexed name, address owner, address addr);
 
     function register(string name, address owner, bytes signature) external;
+    function hash(string name, address owner) public returns (bytes32);
 }
