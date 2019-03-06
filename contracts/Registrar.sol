0f91b267e1ad21979152f2fc20fb770b43f25674
Nick Johnson <arachnid@notdot.net>
2019-03-07 10:48:24 +1300

    Make the registrar set a default resolver and addr record

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -3,20 +3,38 @@ pragma solidity ^0.5.0;
 import "./RegistrarInterface.sol";
 import "./Libraries/SignatureValidator.sol";
 import "@ensdomains/ens/contracts/ENS.sol";
+import "@ensdomains/resolver/contracts/PublicResolver.sol";
 
 contract Registrar is RegistrarInterface {
 
     ENS public ens;
     bytes32 public node;
+    PublicResolver public resolver;
 
-    constructor(ENS _ens, bytes32 _node) public {
+    constructor(ENS _ens, bytes32 _node, PublicResolver _resolver) public {
         ens = _ens;
         node = _node;
+        resolver = _resolver;
     }
 
     function register(string calldata name, address owner, bytes calldata signature) external {
         address token = SignatureValidator.recover(_hash(name, owner), signature);
-        ens.setSubnodeOwner(node, keccak256(abi.encodePacked(name)), owner);
+
+        bytes32 label = keccak256(bytes(name));
+        bytes32 subnode = keccak256(abi.encodePacked(node, label));
+
+        // Create the subdomain and assign it to us.
+        ens.setSubnodeOwner(node, label, this);
+
+        // Set a resolver
+        ens.setResolver(subnode, resolver);
+
+        // Set the resolver's addr record to the new owner
+        resolver.setAddr(subnode, owner);
+
+        // Transfer ownership of the subdomain to the new owner.
+        ens.setOwner(subnode, owner);
+
         emit Registration(keccak256(abi.encodePacked(name)), owner, token);
     }
 
