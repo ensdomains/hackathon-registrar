abd24724bc20c26ca8e5bbef9fd466156240c2b2
Nick Johnson <arachnid@notdot.net>
2019-03-07 18:37:50 +1300

    Don't index the name in the emitted event

--- contracts/RegistrarInterface.sol
+++ contracts/RegistrarInterface.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 interface RegistrarInterface {
 
-    event Registration(bytes32 indexed name, address owner, address addr);
+    event Registration(bytes32 name, address owner, address addr);
 
     function register(string calldata name, address owner, bytes calldata signature) external;
     function hash(string calldata name, address owner) external pure returns (bytes32);
